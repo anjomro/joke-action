46ce76a997bc2972849eb578986f5b3b8bc0e01e
anjomro <github@anjomro.de>
2020-05-03 00:41:54 +0200

    Escaped newlines for GitHub-Action-Output

--- entry.sh
+++ entry.sh
@@ -5,4 +5,7 @@ echo "********************************************************************"
 echo -e "\n\n\n\n\n\n\nThis joke is for $1 !"
 pipenv run python entry.py moo
 JOKE="$(pipenv run python entry.py)"
+JOKE="${JOKE//'%'/'%25'}"
+JOKE="${JOKE//$'\n'/'%0A'}"
+JOKE="${JOKE//$'\r'/'%0D'}"
 echo -e "::set-output name=joke::$JOKE"
